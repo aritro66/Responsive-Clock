6d57e778ab13e41a9274afb8a5d767ae0d00a6f6
aritro66 <78261928+aritro66@users.noreply.github.com>
2021-07-24 12:46:29 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,22 @@
+setInterval(()=>{
+
+    let hour=document.getElementById('hour');
+    let minute=document.getElementById('minute');
+    let second=document.getElementById('second');
+    let date=new Date();
+    let h=date.getHours();
+    let m=date.getMinutes();
+    let s=date.getSeconds();
+    let dh=(h<10)?'0'+String(h):(h<=12)?String(h):String(h-12);
+    let dm=(m>=10)?String(m):("0"+String(m));
+    let ds=(s>=10)?String(s):("0"+String(s));
+    let time_am_pm=(h>=12)?'PM':'AM';
+
+    document.getElementById('content').innerHTML=`${dh+':'+dm+':'+ds+' '+time_am_pm}`;
+    
+    hour.style.transform=`rotate(${h*30+m/2}deg)`;
+    minute.style.transform=`rotate(${m*6}deg)`;
+    second.style.transform=`rotate(${s*6}deg)`;
+
+}
+    ,);
