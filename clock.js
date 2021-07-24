d7bdb7c47be4713078689851ca21f900b1ea453d
aritro66 <78261928+aritro66@users.noreply.github.com>
2021-07-24 15:23:01 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -7,7 +7,7 @@ setInterval(()=>{
     let h=date.getHours();
     let m=date.getMinutes();
     let s=date.getSeconds();
-    let dh=(h<10)?'0'+String(h):(h<=12)?String(h):String(h-12);
+    let dh=(h<10)?'0'+String(h):(h<=12)?String(h):(h-12<10)?("0"+String(h-12)):String(h-12);
     let dm=(m>=10)?String(m):("0"+String(m));
     let ds=(s>=10)?String(s):("0"+String(s));
     let time_am_pm=(h>=12)?'PM':'AM';
